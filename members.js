32477a1343a7df893ded3548595d534426201297
nahrary <navid.ahrary@outlook.com>
2024-05-27 11:37:20 +0000

    Copilot second commit

--- members.js
+++ members.js
@@ -1,19 +1,4 @@
 function skillsMember() {
-    var member = document.getElementById('member').value;
-    var member = member.toLowerCase();
-    switch (member) {
-        case "ronaldo":
-            document.getElementById('skills').innerHTML = "Ronaldo is a good football player";
-            break;
-        case "messi":
-            document.getElementById('skills').innerHTML = "Messi is a good football player";
-            break;
-        case "neymar":
-            document.getElementById('skills').innerHTML = "Neymar is a good football player";
-            break;
-        default:
-            document.getElementById('skills').innerHTML = "Sorry, we don't have information about this player";
-    }
-
-    return false;
+    var skills = ["HTML", "CSS", "JavaScript", "React", "Node.js"];
+    return skills;
 }
